485d0cb0cdd9262fc8222eed5a7599797f89e2dd
Vimal K. Vishwakarma <40199448+vimalcvs@users.noreply.github.com>
2020-12-02 14:33:49 +0530

    Update sliderjsonoutput.php

--- sliderjsonoutput.php
+++ sliderjsonoutput.php
@@ -98,4 +98,380 @@
         "description": "Third of three slide",
         "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
     }
+,
+    {
+        "id": "2",
+        "title": "Slide 2",
+        "description": "Second of three slide",
+        "image_url": "https://www.w3schools.com/howto/img_nature_wide.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "2",
+        "title": "Slide 2",
+        "description": "Second of three slide",
+        "image_url": "https://www.w3schools.com/howto/img_nature_wide.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "2",
+        "title": "Slide 2",
+        "description": "Second of three slide",
+        "image_url": "https://www.w3schools.com/howto/img_nature_wide.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "2",
+        "title": "Slide 2",
+        "description": "Second of three slide",
+        "image_url": "https://www.w3schools.com/howto/img_nature_wide.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "2",
+        "title": "Slide 2",
+        "description": "Second of three slide",
+        "image_url": "https://www.w3schools.com/howto/img_nature_wide.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "2",
+        "title": "Slide 2",
+        "description": "Second of three slide",
+        "image_url": "https://www.w3schools.com/howto/img_nature_wide.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "2",
+        "title": "Slide 2",
+        "description": "Second of three slide",
+        "image_url": "https://www.w3schools.com/howto/img_nature_wide.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "2",
+        "title": "Slide 2",
+        "description": "Second of three slide",
+        "image_url": "https://www.w3schools.com/howto/img_nature_wide.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+,
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
 ]
