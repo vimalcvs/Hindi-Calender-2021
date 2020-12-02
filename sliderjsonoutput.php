02203db352bb39274b2781d34f7fb69d5743611e
Vimal K. Vishwakarma <40199448+vimalcvs@users.noreply.github.com>
2020-12-02 09:57:38 +0530

    Create sliderjsonoutput.php

---
+++ sliderjsonoutput.php
@@ -0,0 +1,20 @@
+[
+    {
+        "id": "1",
+        "title": "Slide 1",
+        "description": "First of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    },
+    {
+        "id": "2",
+        "title": "Slide 2",
+        "description": "Second of three slide",
+        "image_url": "https://www.w3schools.com/howto/img_nature_wide.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://slidesjs.com/img/example-slide-350-1.jpg"
+    }
+]
